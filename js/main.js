56db724a014cd58351aec30f822979f441071c02
Daniel Reynolds <dreynolds@tifcohotels.ie>
2022-05-26 12:40:10 +0100

    Changes 26 May

--- js/main.js
+++ js/main.js
@@ -172,32 +172,31 @@ function initMap() {
         }]
           });  
     var locations = [
-        ['<h3>Crowne Plaza Dublin Airport</h3> <p>Park Northwood<br/>Santry<br/>Dublin 9<br/>Ireland<br/>D09 X9X2</p>', 53.404402, -6.245599, 'http://www.tifcohotels.ie/_fileUpload/Image/cp_da.png'],
-        ['<h3>Holiday Inn Express Dublin Airport</h3> <p>Northwood Park<br/>NorthWood<br/>Dublin 9<br/>Ireland<br/>D09 RY17</p>', 53.404275, -6.244526, 'http://www.tifcohotels.ie/_fileUpload/Image/hiExpress.png'],
-        ['<h3>Crowne Plaza Dublin - Blanchardstown</h3> <p>The Blanchardstown Centre<br/>Dublin 15<br/>Ireland<br/>D15 T1FD</p>', 53.395652, -6.389966, 'http://www.tifcohotels.ie/_fileUpload/Image/cp_b.png'],
-        ['<h3>Clontarf Castle Hotel</h3> <p>Castle Ave<br/>Clontarf<br/>Dublin 3<br/>Ireland<br/>D03 W5N0</p>', 53.364754, -6.206846, 'http://www.tifcohotels.ie/_fileUpload/Image/clontarf_logo.png'],
-        ['<h3>Hilton Dublin Kilmainham</h3> <p>South Circular Road<br/>Kilmainham<br/>Dublin 8<br/>Ireland<br/>D08 XAK3</p>', 53.342411, -6.308169, 'http://www.tifcohotels.ie/_fileUpload/Image/hilton.png'],
-        ['<h3>Parliament Hotel</h3> <p>16/18 Lord Edward Street<br/>Dublin 2<br/>Ireland<br/>D02 TD73</p>', 53.344103, -6.268006, 'http://www.tifcohotels.ie/_fileUpload/Image/parliament.png'],
-        ['<h3>Crowne Plaza Dundalk</h3> <p>Inner Relief Road<br/>Dundalk<br/>Co. Louth<br/>Ireland<br/>A91 EF88</p>', 53.9803991, -6.3906617, 'http://www.tifcohotels.ie/_fileUpload/Image/cp_dk.png'],
-        ['<h3>The Heritage Killenard</h3> <p>Killenard<br/>Co. Laois<br/>Ireland<br/>R32 PW10</p>', 53.133603, -7.151670, 'http://www.tifcohotels.ie/_fileUpload/Image/heritage.png'],
-        ['<h3>Athlone Springs Hotel</h3> <p>Athlone<br/>Co. Westmeath<br/>Ireland<br/>N37 F9T3</p>', 53.421144, -7.997189, 'http://www.tifcohotels.ie/_fileUpload/Image/athlone_logo.png'],
-        ['<h3>Hotel Killarney</h3> <p>Cork Road<br/>Killarney<br/>Ireland<br/>V93 FX00</p>', 52.060288, -9.484677, 'http://www.tifcohotels.ie/_fileUpload/Image/killarney.png'],
-        ['<h3>Innisfallen</h3><p>Fossa<br/>Killarney<br/>Co. Kerry<br/>Ireland<br/>V93 FX00</p>', 52.070643, -9.573754, 'http://www.tifcohotels.ie/_fileUpload/Image/innisfallen-icon-map.png'],
-        ['<h3>The Metropole Hotel</h3> <p>MacCurtain Street<br/>Cork City<br/>Ireland<br/>T23 EEC3</p>', 51.901319, -8.467594, 'http://www.tifcohotels.ie/_fileUpload/Image/metropole.png'],
-        ['<h3>Cork International Hotel</h3> <p>Cork Airport Business Park<br/>Cork<br/>Co. Cork<br/>Ireland<br/>T12 H516</p>', 51.852083, -8.486338, 'http://www.tifcohotels.ie/_fileUpload/Image/cork_international.png'],
-        ['<h3>Cork Airport Hotel</h3> <p>Cork Airport<br/>Co. Cork<br/>Ireland<br/>T12 RPP9</p>', 51.848014, -8.486059, 'http://www.tifcohotels.ie/_fileUpload/Image/cork_airport.png'],
-        ['<h3>Travelodge Waterford Hotel</h3> <p>N25 Cork Road<br/>Waterford<br/>Co. Waterford<br/>Ireland<br/>X91 YV04</p>', 52.248430, -7.121480, 'http://www.tifcohotels.ie/_fileUpload/Image/travelodge.png'],
-        ['<h3>Travelodge Limerick Ennis Road Hotel</h3> <p>Coonagh Roundabout<br/>Ennis Road<br/>N18<br/>Limerick<br/>Ireland<br/>V94 R990</p>', 52.673351, -8.675873, 'http://www.tifcohotels.ie/_fileUpload/Image/travelodge.png'],
-        ['<h3>Travelodge Limerick Castletroy Hotel</h3> <p>Park Point<br/>Castletroy<br/>Dublin Road<br/>Limerick<br/>Ireland<br/>V94 Y83C</p>', 52.664580, -8.585064, 'http://www.tifcohotels.ie/_fileUpload/Image/travelodge.png'],
-        ['<h3>Travelodge Galway City Hotel</h3> <p>Joyce Roundabout<br/>Galway City Centre<br/>Tuam Road Galway<br/>Ireland<br/>H91 HX8Y</p>', 53.282843, -9.036276, 'http://www.tifcohotels.ie/_fileUpload/Image/travelodge.png'],
-        ['<h3>Travelodge Dublin Phoenix Park Hotel</h3> <p>Auburn Avenue Roundabout<br/>Navan Road<br/>Dublin 15<br/>Ireland<br/>D15 EW29</p>', 53.380359, -6.355205, 'http://www.tifcohotels.ie/_fileUpload/Image/travelodge.png'],
-        ['<h3>Travelodge Dublin City Centre</h3> <p>Stephens Green Hotel<br/>Mercer Street Lower<br/>Dublin 2<br/>Ireland<br/>D02 TW61</p>', 53.339576, -6.264111, 'http://www.tifcohotels.ie/_fileUpload/Image/travelodge.png'],
-        ['<h3>Travelodge Dublin City Centre Rathmines</h3> <p>Rathmines Road<br/>Dublin 6<br/>Ireland<br/>D06 HF30</p>', 53.325093, -6.265007, 'http://www.tifcohotels.ie/_fileUpload/Image/travelodge.png'],
-        ['<h3>Travelodge Dublin Airport South Hotel</h3> <p>Shangan Road<br/>Ballymun<br/>Dublin 9<br/>Ireland<br/>D09 W8H7</p>', 53.396499, -6.263210, 'http://www.tifcohotels.ie/_fileUpload/Image/travelodge.png'],
-        ['<h3>Travelodge Dublin Airport North Swords Hotel</h3> <p>Pinnock Hill Roundabout<br/>Swords<br/>Co. Dublin<br/>Ireland<br/>K67 K6R2</p>', 53.449318, -6.223605, 'http://www.tifcohotels.ie/_fileUpload/Image/travelodge.png'],
-        ['<h3>Travelodge Derry Hotel</h3> <p>22-24 Strand Road<br/>Derry<br/>Derry<br/>Derry<br/>United Kingdom<br/>BT48 7AB</p>', 54.999520, -7.321776, 'http://www.tifcohotels.ie/_fileUpload/Image/travelodge.png'],
-        ['<h3>Travelodge Cork Airport Hotel</h3> <p>Black Ash<br/>Kinsale Road Roundabout<br/>Frankfield Road<br/>Co. Cork<br/>Ireland<br/>T12 E2XY</p>', 51.872464, -8.470073, 'http://www.tifcohotels.ie/_fileUpload/Image/travelodge.png'],
-        ['<h3>Travelodge Belfast Central Hotel</h3> <p>15 Brunswick Street<br/>Belfast<br/>Belfast<br/>United Kingdom<br/>BT2 7GE</p>', 54.595178, -5.933368, 'http://www.tifcohotels.ie/_fileUpload/Image/travelodge.png']
+        ['<h3>Crowne Plaza Dublin Airport</h3> <p>Park Northwood<br/>Santry<br/>Dublin 9<br/>Ireland<br/>D09 X9X2</p>', 53.404402, -6.245599, 'https://www.tifcohotels.ie/_fileUpload/Image/cp_da.png'],
+        ['<h3>Holiday Inn Express Dublin Airport</h3> <p>Northwood Park<br/>NorthWood<br/>Dublin 9<br/>Ireland<br/>D09 RY17</p>', 53.404275, -6.244526, 'https://www.tifcohotels.ie/_fileUpload/Image/hiExpress.png'],
+        ['<h3>Crowne Plaza Dublin - Blanchardstown</h3> <p>The Blanchardstown Centre<br/>Dublin 15<br/>Ireland<br/>D15 T1FD</p>', 53.395652, -6.389966, 'https://www.tifcohotels.ie/_fileUpload/Image/cp_b.png'],
+        ['<h3>Clontarf Castle Hotel</h3> <p>Castle Ave<br/>Clontarf<br/>Dublin 3<br/>Ireland<br/>D03 W5N0</p>', 53.364754, -6.206846, 'https://www.tifcohotels.ie/_fileUpload/Image/clontarf_logo.png'],
+        ['<h3>Hilton Dublin Kilmainham</h3> <p>South Circular Road<br/>Kilmainham<br/>Dublin 8<br/>Ireland<br/>D08 XAK3</p>', 53.342411, -6.308169, 'https://www.tifcohotels.ie/_fileUpload/Image/hilton.png'],
+        ['<h3>The Hendrick Smithfield</h3> <p>6 – 11 Hendrick Street<br/>Dublin 7<br/>Ireland<br/>D07 KX28</p>', 53.347805, -6.280856, 'https://www.tifcohotels.ie/_fileUpload/Image/map-icon-hnrk.png'],
+        ['<h3>Crowne Plaza Dundalk</h3> <p>Inner Relief Road<br/>Dundalk<br/>Co. Louth<br/>Ireland<br/>A91 EF88</p>', 53.9803991, -6.3906617, 'https://www.tifcohotels.ie/_fileUpload/Image/cp_dk.png'],
+        ['<h3>Hotel Killarney</h3> <p>Cork Road<br/>Killarney<br/>Ireland<br/>V93 FX00</p>', 52.060288, -9.484677, 'https://www.tifcohotels.ie/_fileUpload/Image/killarney.png'],
+        ['<h3>Travelodge Waterford Hotel</h3> <p>N25 Cork Road<br/>Waterford<br/>Co. Waterford<br/>Ireland<br/>X91 YV04</p>', 52.248430, -7.121480, 'https://www.tifcohotels.ie/_fileUpload/Image/travelodge.png'],
+        ['<h3>Travelodge Limerick Ennis Road Hotel</h3> <p>Coonagh Roundabout<br/>Ennis Road<br/>N18<br/>Limerick<br/>Ireland<br/>V94 R990</p>', 52.673351, -8.675873, 'https://www.tifcohotels.ie/_fileUpload/Image/travelodge.png'],
+        ['<h3>Travelodge Limerick Castletroy Hotel</h3> <p>Park Point<br/>Castletroy<br/>Dublin Road<br/>Limerick<br/>Ireland<br/>V94 Y83C</p>', 52.664580, -8.585064, 'https://www.tifcohotels.ie/_fileUpload/Image/travelodge.png'],
+        ['<h3>Travelodge Galway City Hotel</h3> <p>Joyce Roundabout<br/>Galway City Centre<br/>Tuam Road Galway<br/>Ireland<br/>H91 HX8Y</p>', 53.282843, -9.036276, 'https://www.tifcohotels.ie/_fileUpload/Image/travelodge.png'],
+        ['<h3>Travelodge Dublin Phoenix Park Hotel</h3> <p>Auburn Avenue Roundabout<br/>Navan Road<br/>Dublin 15<br/>Ireland<br/>D15 EW29</p>', 53.380359, -6.355205, 'https://www.tifcohotels.ie/_fileUpload/Image/travelodge.png'],
+        ['<h3>Travelodge Dublin City Centre Rathmines</h3> <p>Rathmines Road<br/>Dublin 6<br/>Ireland<br/>D06 HF30</p>', 53.325093, -6.265007, 'https://www.tifcohotels.ie/_fileUpload/Image/travelodge.png'],
+        ['<h3>Travelodge Plus Dublin City Centre</h3> <p>44 Townsend St<br/>Dublin 2<br/>Ireland<br/>D02 DY01</p>', 53.34600, -6.25204, 'https://www.tifcohotels.ie/_fileUpload/Image/travelodge.png'],
+        ['<h3>Travelodge Dublin Airport South Hotel</h3> <p>Shangan Road<br/>Ballymun<br/>Dublin 9<br/>Ireland<br/>D09 W8H7</p>', 53.396499, -6.263210, 'https://www.tifcohotels.ie/_fileUpload/Image/travelodge.png'],
+        ['<h3>Travelodge Dublin Airport North Swords Hotel</h3> <p>Pinnock Hill Roundabout<br/>Swords<br/>Co. Dublin<br/>Ireland<br/>K67 K6R2</p>', 53.449318, -6.223605, 'https://www.tifcohotels.ie/_fileUpload/Image/travelodge.png'],
+        ['<h3>Travelodge Cork Airport Hotel</h3> <p>Black Ash<br/>Kinsale Road Roundabout<br/>Frankfield Road<br/>Co. Cork<br/>Ireland<br/>T12 E2XY</p>', 51.872464, -8.470073, 'https://www.tifcohotels.ie/_fileUpload/Image/travelodge.png'],
+        ['<h3>Travelodge Belfast Central Hotel</h3> <p>15 Brunswick Street<br/>Belfast<br/>Belfast<br/>United Kingdom<br/>BT2 7GE</p>', 54.595178, -5.933368, 'https://www.tifcohotels.ie/_fileUpload/Image/travelodge.png'],
+        ['<h3>Kinsale Hotel & Spa</h3> <p>Rathmore, Kinsale, County Cork, P17 F542</p>', 51.698865, -8.465479, 'https://www.tifcohotels.ie/_fileUpload/Image/KinsaleHotel.png'],                     
+        ['<h3>Innisfallen Hotel</h3> <p>Fossa<br/>Killarney<br/>Co. Kerry<br/>Ireland<br/>V93 CR90</p>', 52.070643, -9.573754, 'https://www.tifcohotels.ie/_fileUpload/Image/innisfallen-icon-map.png'],
+        ['<h3>The Clybaun Hotel</h3> <p>Clybaun Road<br/>Knocknacarra<br/>Co. Galway<br/>Ireland<br/>H91 F62V</p>', 53.269480, -9.108403, 'https://www.tifcohotels.ie/_fileUpload/Image/clybaun_map.png'],
+        ['<h3>Hard Rock Dublin Hotel</h3> <p>18 Exchange Street Upper<br/>Dublin 2<br/>Ireland<br/>D08 AV24</p>', 53.344286, -6.268025, 'https://www.tifcohotels.ie/_fileUpload/Image/map-icon-hrd.png'],
+        ['<h3>Arthaus Hotel Dublin</h3> <p>Mercer Street Lower<br/>Dublin 2<br/>Ireland<br/>D02 TW61</p>', 53.339552834177255, -6.264078911187995, 'https://www.tifcohotels.ie/_fileupload/Arthaus-map-icon(1).png']                                                      
+
     ];
     
     var infowindow = new google.maps.InfoWindow();
@@ -221,3 +220,121 @@ function initMap() {
         var markerCluster = new MarkerClusterer(map, markers,
             {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});   
       }
+
+
+
+$('.image_box').click(function(){
+
+    $(".black_box").css("height","100%");
+
+    // Read Text Values
+    var dataheading = $(this).find("h2").text();
+    var dataImg = $(this).find(".hidden_values img").attr('src');
+    var dataP = $(this).find(".p_text_block").text();
+
+
+    // Write New Text Values
+    $('.hotel_row img').attr("src", dataImg);
+    $('.hotel_row .p_text_block').text(dataP);
+    $('.hotel_row h2').text(dataheading); 
+    
+
+setTimeout(function(){ 
+
+    $(".overlay").fadeIn();
+    $('.inis.image_box p').hide();
+
+}, 200); 
+
+    
+
+});
+
+$('.close_overlay').click(function(){
+
+    $(".overlay").fadeOut();    
+
+    setTimeout(function(){ 
+
+    $(".black_box").css("height","0");
+
+}, 250); 
+
+});
+
+$('.image_box').mouseover(function(){
+
+  var overlay = $(this).find('.fill');
+  overlay.css("opacity","0.7");
+
+  
+
+
+    var getButton = $(this).find('.btn-second');
+    getButton.css("bottom","0px");
+
+
+    
+  
+});
+
+$('.image_box').mouseleave(function(){
+
+    
+    $('.image_box p').fadeOut();
+    $('.fill').css("opacity","0.5");
+    $('.btn-second').css("bottom","-50px");
+
+
+
+});
+
+/*
+
+$('.image_box').mouseover(function(){
+
+  $('.fill').css("opacity","0.7");
+  
+
+    setTimeout(function(){ 
+
+      $('.image_box p').css("display","block");
+
+    }, 500);  
+
+});
+
+$('.image_box').mouseleave(function(){
+
+    
+    $('.image_box p').fadeOut();
+
+    setTimeout(function(){ 
+
+
+    }, 500); 
+
+});
+
+
+$('.readmore').click(function(){
+
+    $(".black_box").css("height","100%"); 
+    
+
+setTimeout(function(){ 
+
+    $(".overlay").fadeIn();
+    $('.inis.image_box p').hide();
+
+}, 200); 
+
+    
+
+});
+
+
+
+*/
+
+
