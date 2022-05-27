9f6a4ebc9c5fdac660afccc02776503d90b8288e
Daniel Reynolds <dreynolds@tifcohotels.ie>
2022-05-27 14:41:12 +0100

    Changes 26 May Timeline 2

---
+++ js/extra-script.js
@@ -0,0 +1,20 @@
+    
+    $(".sub").hide();
+
+    $(".sub_sub").hide();
+
+    $(".open_sub").click(function(){
+
+    $(".sub").slideToggle();
+
+    });
+
+    $(".open_sub_sub").click(function(){
+
+    $(".sub_sub").slideToggle();
+
+    });
+
+
+
+	
