0d15c8c007ba235ddbaf724eacccefb9bb4215aa
Daniel Reynolds <dreynolds@tifcohotels.ie>
2022-07-12 13:34:34 +0100

    revamp - banners 2

--- js/extra-script.js
+++ js/extra-script.js
@@ -16,5 +16,46 @@
     });
 
 
+$('.accordion_head').click(function(){
+
+	// make open head
+
+	if (!$(this).hasClass('open')){
+
+		$(this).addClass('open');
+
+	}
+
+	var parent = $(this).parent();
+
+	var nearest = parent.find('.accordion_body');
+
+
+
+	nearest.slideDown();
+	nearest.addClass('open');
+	
+
+    //$('.accordion_body').slideDown();
+
+
+});
+
+$('.accordion_body .bolder').click(function(){
+
+	var parent_2 = $(this).parent();
+
+	var nearest_2 = parent_2.find('.innerbody');
+	
+	nearest_2.slideDown();
+	nearest_2.addClass('open');
+	
+
+});
+
+
+
+
+
 
 	
