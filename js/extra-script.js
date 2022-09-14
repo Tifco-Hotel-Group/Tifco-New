0c297fa965ba7143171cf73d87674a4408c01dfc
Daniel Reynolds <dreynolds@tifcohotels.ie>
2022-09-14 15:22:39 +0100

    content updates

--- js/extra-script.js
+++ js/extra-script.js
@@ -16,7 +16,28 @@
     });
 
 
+$('.accordion_body').click(function(){
+
+	if (!$(this).hasClass('open')){
+
+		$(this).removeClass('open');
+		$(this).slideUp();
+
+	}
+	else
+	{
+		$(this).addClass('open');
+		$(this).slideDown();
+
+	}
+
+});
+
+
 $('.accordion_head').click(function(){
+
+		var parent = $(this).parent();
+		var nearest = parent.find('.accordion_body');
 
 	// make open head
 
@@ -24,16 +45,23 @@ $('.accordion_head').click(function(){
 
 		$(this).addClass('open');
 
+		
+		nearest.slideDown();
+		nearest.addClass('open');
+
 	}
 
-	var parent = $(this).parent();
+	else{
 
-	var nearest = parent.find('.accordion_body');
+		$(this).removeClass('open');
+
+		nearest.slideUp();
+		nearest.removeClass('open');
 
 
+	}
 
-	nearest.slideDown();
-	nearest.addClass('open');
+	
 	
 
     //$('.accordion_body').slideDown();
@@ -57,5 +85,72 @@ $('.accordion_body .bolder').click(function(){
 
 
 
+var accordion = (function(){
+  
+  var $accordion = $('.js-accordion');
+  var $accordion_header = $accordion.find('.js-accordion-header');
+  var $accordion_item = $('.js-accordion-item');
+ 
+  // default settings 
+  var settings = {
+    // animation speed
+    speed: 100,
+    
+    // close all other accordion items if true
+    oneOpen: false
+  };
+    
+  return {
+    // pass configurable object literal
+    init: function($settings) {
+      $accordion_header.on('click', function() {
+        accordion.toggle($(this));
+      });
+      
+      $.extend(settings, $settings); 
+      
+      // ensure only one accordion is active if oneOpen is true
+      if(settings.oneOpen && $('.js-accordion-item.active').length > 1) {
+        $('.js-accordion-item.active:not(:first)').removeClass('active');
+      }
+      
+      // reveal the active accordion bodies
+      $('.js-accordion-item.active').find('> .js-accordion-body').show();
+    },
+    toggle: function($this) {
+            
+      if(settings.oneOpen && $this[0] != $this.closest('.js-accordion').find('> .js-accordion-item.active > .js-accordion-header')[0]) {
+        $this.closest('.js-accordion')
+               .find('> .js-accordion-item') 
+               .removeClass('active')
+               .find('.js-accordion-body')
+               .slideUp('fast')
+      }
+      
+      // show/hide the clicked accordion item
+      $this.closest('.js-accordion-item').toggleClass('active');
+      $this.next().stop().slideToggle(settings.speed);
+    }
+  }
+})();
 
+$(document).ready(function(){
+  accordion.init({ speed: 300, oneOpen: true });
+});
 	
+
+// Count Jobs
+
+$(".hotel_name").click(function(){
+
+var hotelThis = $(this).text();
+var parent = $(this).parent();
+var jobsHere = parent.find('.Opportunity_item').length;
+var announcementThis = hotelThis + " currently has " + jobsHere + " job listings."
+parent.find(".hotel_listings").text(announcementThis);
+
+
+});
+
+var alljobs = $('.Opportunity_item').length;
+
